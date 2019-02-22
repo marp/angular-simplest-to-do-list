c8d63f662b857af35b47f54abf1fad66abec59d7
me <szybkimail@o2.pl>
2019-02-22 18:18:13 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todolist';
+  newTask: string;
+  taskList: Array<string> = [];
+  doneList: Array<string> = [];
+
+  add() {
+    this.taskList.push(this.newTask);
+    this.newTask = '';
+  }
+
+  remove(task: string, index: number) {
+    // e in filter mean each element
+    this.taskList = this.taskList.filter( (e, i) => e !== task || i !== index  );
+  }
+
+  done(task: string, index: number) {
+    this.doneList.push(task);
+    this.remove(task, index);
+  }
+
+
 }
